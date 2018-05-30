df1e88f17cd70fbd31ec22de038f52ca94b32ada
Remco van den Berg <remco@fritzy.nl>
2018-05-30 17:58:47 +0200

    introducing dotnet

---
+++ Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace lucidity
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            app.Use( async(context, next) => 
+            {
+                await next();
+                if (context.Response.StatusCode == 404 && !Path.HasExtension(
+                    context.Request.Path.Value) && !context.Request.Path.StartsWithSegments("api"))
+                    {
+                        context.Request.Path = "/index.html";
+                        context.Response.StatusCode = 200;
+                        await next();
+                    }   
+            });
+        }
+    }
+}
